3291d53051625940fe229c7855eece453f7e1ca3
Agustinus Kristiadi <kristiadi@protonmail.com>
2018-05-22 11:17:41 +0200

    Initial commit: full, diagonal, KFAC

---
+++ README.md
@@ -0,0 +1,14 @@
+# Natural Gradients
+Collection of algorithms for approximating Fisher Information Matrix for Natural Gradient. The problems used are the simplest possible (e.g. toy classification problem and MNIST), to highlight more on the algorithms themshelves.
+
+### Dependencies
+
+1. Python 3.5+
+2. Numpy
+3. Matplotlib
+
+## List of Algorithms
+
+1. Exact Fisher
+2. Diagonal Fisher
+3. KFAC (Kronecker-factored Approximate Curvature) [[pdf]](http://www.cs.toronto.edu/~rgrosse/icml2015-kfac.pdf)
---
+++ logreg_fang.py
@@ -0,0 +1,57 @@
+import numpy as np
+from sklearn.utils import shuffle
+
+
+np.random.seed(9999)
+
+X0 = np.random.randn(100, 2) - 1
+X1 = np.random.randn(100, 2) + 1
+X = np.vstack([X0, X1])
+t = np.vstack([np.zeros([100, 1]), np.ones([100, 1])])
+
+X, t = shuffle(X, t)
+
+X_train, X_test = X[:150], X[:50]
+t_train, t_test = t[:150], t[:50]
+
+# Model
+W = np.random.randn(2, 1) * 0.01
+
+
+def sigm(x):
+    return 1/(1+np.exp(-x))
+
+
+def NLL(y, t):
+    return -np.mean(t*np.log(y) + (1-t)*np.log(1-y))
+
+
+alpha = 0.1
+
+# Training
+for it in range(5):
+    # Forward
+    z = X_train @ W
+    y = sigm(z)
+    loss = NLL(y, t_train)
+
+    # Loss
+    print(f'Loss: {loss:.3f}')
+
+    m = y.shape[0]
+
+    dy = (y-t_train)/(m * (y - y*y))
+    dz = sigm(z)*(1-sigm(z))
+    dW = X_train.T @ (dz * dy)
+
+    grad_loglik_z = (t_train-y)/(y - y*y) * dz
+    grad_loglik_W = grad_loglik_z * X_train
+    F = np.cov(grad_loglik_W.T)
+
+    # Step
+    W = W - alpha * np.linalg.inv(F) @ dW
+
+y = sigm(X_test @ W).ravel()
+acc = np.mean((y >= 0.5) == t_test.ravel())
+
+print(f'Accuracy: {acc:.3f}')
